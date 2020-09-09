5b5eeb9e9ba7f75bd23d5bad05c490e4fbc2a289
Hung Le <hung@sentius.com.au>
2020-09-09 16:28:36 +0700

    FLG: update payment require expire in 48 hours

--- sites/all/themes/mytheme/templates/html.tpl.php
+++ sites/all/themes/mytheme/templates/html.tpl.php
@@ -10,8 +10,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   
-  
-  
 	<title><?php print $head_title; ?></title>
 	<?php print $styles; ?>
 
